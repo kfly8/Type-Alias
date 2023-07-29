56460b0bb8217c4e27b0089ce6a703f0a9ccf3fb
kobaken <kentafly88@gmail.com>
2023-07-29 10:14:13 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Type::Alias - It's new $module
+
+# SYNOPSIS
+
+    use Type::Alias;
+
+# DESCRIPTION
+
+Type::Alias is ...
+
+# LICENSE
+
+Copyright (C) kobaken.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+kobaken <kentafly88@gmail.com>
---
+++ lib/Type/Alias.pm
@@ -0,0 +1,39 @@
+package Type::Alias;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Type::Alias - It's new $module
+
+=head1 SYNOPSIS
+
+    use Type::Alias;
+
+=head1 DESCRIPTION
+
+Type::Alias is ...
+
+=head1 LICENSE
+
+Copyright (C) kobaken.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+kobaken E<lt>kentafly88@gmail.comE<gt>
+
+=cut
+
